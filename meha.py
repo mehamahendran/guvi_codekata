50d1ac329ad756d71e8e8c3600396e913eedbf13
mehamahendran <36658894+mehamahendran@users.noreply.github.com>
2018-02-20 14:34:50 +0530

    Add files via upload

---
+++ meha.py
@@ -0,0 +1,10 @@
+def main():
+    x=int(input("enter a number:"))
+    if(x>0):
+        print("number is positive")
+    if(x==0):
+        print("number is zero")
+    else: 
+        print("number is negative")
+main()        
+        
